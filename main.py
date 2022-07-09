a7776a8a25d6c28d252a32c52edf88bf2ec7e40e
Murilo Caires <37487013+murilocaires@users.noreply.github.com>
2022-07-08 22:51:07 -0300

    Update main.py
    prints atualizados

--- main.py
+++ main.py
@@ -1,22 +1,25 @@
+
+
 from mechanics import *
 
 load()
 
 while True:
-    print('\n')
-    print('(1) Agendar')
-    print('(2) - Listar alarmes')
-    print('(3) - Sobre')
-    print('(0) - Sair')
-    print('\n')
-
+    print(30*'-=')
+    print('   -> (1) - Agendar ')
+    print('   -> (2) - Listar alarmes  ')
+    print('   -> (3) - Sobre           ')
+    print('   -> (0) - Sair   ')
+    print(30 * '-=')
     option = input('Selecione: ').lower()
     if option == '1':
         schedule = create_schedule()
         if schedule is None:
+            print(30 * '-=')
             print('\nNão foi possível agendar o alarme. Ocorreu algo inesperado.\n')
         else:
-            print('\nAlarme agendado com sucesso!\n')
+            print(30 * '-=')
+            print('                Alarme agendado com sucesso!')
     elif option == '2':
         list_schedules()
     elif option == '3':
@@ -24,7 +27,7 @@ while True:
     elif option == '0':
         break
     elif option == 'voltar':
-        print('Voltando...\n')
+        print(' \n                      Voltando...\n')
     else:
         print('Opção inválida!')
         continue
